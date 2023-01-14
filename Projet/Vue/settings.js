f1635f54b41ea279bc9ec530d4c9033b27d5e2a4
Axel TERRIER <47625927+axelterrier@users.noreply.github.com>
2023-01-14 18:59:06 +0100

    Delete Projet directory

--- Projet/Vue/settings.js
+++
@@ -1,7 +0,0 @@
-function isDarkModeOn(){
-    if (document.getElementById('checkBox').checked) {
-        document.getElementById("valueDarkMode").innerHTML = "on";
-    } else {
-        document.getElementById("valueDarkMode").innerHTML = "off";
-    }
-}
